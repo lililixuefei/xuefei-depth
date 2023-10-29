65bec0853ec024464e3a741fcdd23e7b379cca3c
xuefei <13273726905@163.com>
2023-10-29 19:08:12 +0800

    TestReadWriteLock

---
+++ xuefei-concurrent/src/main/java/manshen/aqs/CachedData.java
@@ -0,0 +1,44 @@
+package manshen.aqs;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @Description
+ * @Author xuefei
+ * @Date 2023/10/29 19:01
+ * @Version 1.0
+ */
+public class CachedData {
+
+    Object data;
+    // 是否有效，如果失效，需要重新计算 data
+    volatile boolean cacheValid;
+    final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+
+    void processCachedData() {
+        rwl.readLock().lock();
+        if (!cacheValid) {
+            // 获取写锁前必须释放读锁
+            rwl.readLock().unlock();
+            rwl.writeLock().lock();
+            try {
+                // 判断是否有其它线程已经获取了写锁、更新了缓存, 避免重复更新
+                if (!cacheValid) {
+                    data = "...";
+                    cacheValid = true;
+                }
+                // 降级为读锁, 释放写锁, 这样能够让其它线程读取缓存
+                rwl.readLock().lock();
+            } finally {
+                rwl.writeLock().unlock();
+            }
+        }
+        // 自己用完数据, 释放读锁
+        try {
+//            use(data);
+        } finally {
+            rwl.readLock().unlock();
+        }
+    }
+
+}
